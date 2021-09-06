582ab169a90ebeaa519cc0b9ca10af564ac96a7f
Mavrk24 <84179997+Mavrk24@users.noreply.github.com>
2021-09-06 13:02:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p> Test
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <LambdaDemo />
